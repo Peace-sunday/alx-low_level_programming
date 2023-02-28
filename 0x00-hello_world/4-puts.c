4482bae040b0737833846a357dcc1989017e2295
Peace-sunday <peacesunday0807@gmail.com>
2023-02-28 11:49:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Ending with a value of 0
+ * main - A program printing a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char Multi[] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(Multi);
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
