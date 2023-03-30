b93a4b461d381517d2e744d4f1b1260580015d53
Peace-sunday <peacesunday0807@gmail.com>
2023-03-30 16:59:13 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int num1 = 0, num2 = 0;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("%s\n", "Error");
-		return (1);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
 
 	return (0);
 }
